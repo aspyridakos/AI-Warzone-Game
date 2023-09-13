5f01f9a66be03c03de9769b9fc58ff16574ae8f7
Alexandra <aspyridakos@gmail.com>
2023-09-13 15:43:36 -0400

    adding pylintrc file

--- main.py
+++ main.py
@@ -3,8 +3,16 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+"""
+This is a sample function
+"""
+
 
 def print_hi(name):
+    """
+
+    :param name:
+    """
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
