d0b564ea7286f1a31bf936aae4b27b445d6e67da
Christos Karabatsos <chris.karabatsos5@gmail.com>
2023-09-14 15:09:33 -0400

    Created Board (Base version subject to change)
    Base 5x5 board with white and grey color. Thinking we can change color schemes to have one half a color for attacking and one half a color for defending.

---
+++ WargameMain.py
@@ -0,0 +1,50 @@
+import pygame as p
+
+'''
+Set the dimensions for the separate window and squares shown on board
+'''
+Width = Height = 800
+Dimension = 5
+SquareSize = Height // Dimension
+
+
+'''
+This is the main function to run the whole program and generate everything needed for the UI being 
+shown to the user.
+'''
+def main():
+    p.init()
+    screen = p.display.set_mode((Width, Height))
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        drawBoard(screen)
+        p.display.flip()
+
+
+'''
+board function which creates the colors and where to place the colors on the square blocks 
+based on nested for loops
+'''
+def drawBoard(screen):
+    color = [p.Color("white"), p.Color("gray")]
+    borderThickness = 3
+    for r in range(Dimension):
+        for c in range(Dimension):
+            colors = color[((r + c) % 2)]
+            #calculates the color of the current square based on the sum of the row and column,
+            #alternates based on even or odd number from sum
+            p.draw.rect(screen, colors, p.Rect(c*SquareSize, r*SquareSize, SquareSize, SquareSize))
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
