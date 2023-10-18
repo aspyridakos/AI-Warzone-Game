0ab6d9f8be46323be2069c129321db02e4d593e9
Christos Karabatsos <chris.karabatsos5@gmail.com>
2023-10-18 15:33:21 -0400

    Added e1 heuristic function

--- main.py
+++ main.py
@@ -668,6 +668,8 @@ class Game:
 
         # TODO: set other heuristics here (e1 => aggressive play, e2 => defensive play???)
         initial_heuristic = self.get_heuristic_e0()
+        # initial_heuristic = self.get_heuristic_e1()
+        # initial_heuristic = self.get_heuristic_e2()
 
         # Optimizing for MAX player
         if initial_heuristic > 0:
@@ -726,6 +728,39 @@ class Game:
                      3 * attacker_counts[UnitType.Firewall] + 3 * attacker_counts[UnitType.Program] +
                      9999 * attacker_counts[UnitType.AI]))
         return e0
+
+    def get_heuristic_e1(self) -> int:
+        """Calculate e1 heuristic (more offensive)"""
+        player_unit_counts = {player: defaultdict(int) for player in Player}
+
+        for player in Player:
+            for coord, unit in self.player_units(player):
+                player_unit_counts[player][unit.type] += 1
+
+        attacker_counts = player_unit_counts[Player.Attacker]
+        defender_counts = player_unit_counts[Player.Defender]
+
+        e1: int
+        if self._attacker_has_ai:
+            e1 = (
+                # Prioritize killing pieces that can move/attack when engaged in combat (Tech and Virus)
+                    (10 * (attacker_counts[UnitType.Tech] + 10* attacker_counts[UnitType.Virus])) +
+                # Prioritize keeping your own pieces alive that are more powerful on attack (Firewall and Program)
+                    (5 * (attacker_counts[UnitType.Firewall] + 5 * attacker_counts[UnitType.Program])) -
+                # Always prioritize the AI since it's the game decider
+                    (9999 * attacker_counts[UnitType.AI])
+            )
+        else:
+            e1 = (
+                # Prioritize killing pieces that can move/attack when engaged in combat (Tech and Virus)
+                    (10 * (defender_counts[UnitType.Tech] + 10 * defender_counts[UnitType.Virus])) +
+                # Prioritize keeping your own pieces alive that are more powerful on attack (Firewall and Program)
+                    (5 * (defender_counts[UnitType.Firewall] + 5 * defender_counts[UnitType.Program])) -
+                # Always prioritize the AI since it's the game decider
+                    (9999 * defender_counts[UnitType.AI])
+            )
+        return e1
+
 
     def post_move_to_broker(self, move: CoordPair):
         """Send a move to the game broker."""
